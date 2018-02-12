e8308a6ffe96434a663a03d1543ed0fd223b35c9
Venessa Yeh <32401637+vtyeh@users.noreply.github.com>
2018-02-12 14:52:19 -0800

    python challenges solved

---
+++ python-challenge/PyBank/main.py
@@ -0,0 +1,61 @@
+import os
+import csv
+
+budget_path = os.path.join("raw_data", "budget_data_2.csv")
+
+print("Financial Analysis")
+print("------------------")
+
+#   Create empty lists to store new lists
+total_months = []
+total_revenue =[]
+avg_rev =[]
+
+revenue = 0
+index = 0
+with open(budget_path, 'r', newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvfile, None)
+
+    # Calculate total number of months
+    for row in csvreader:
+        total_months.append(row[0])
+        total = len(list(total_months))
+
+        # Calculate total amount of revenue gained over entire period
+        revenue += int(row[1])
+        total_revenue.append(row[1])
+    print("Total Months: " + str(total))
+    print("Total Revenue: $" + str(revenue))
+
+    # Calculate average change in revenue between months
+    # for x in total_revenue:
+    #     x = int(total_revenue[index]) - int(total_revenue[index - 1])
+    #     index = index + 1
+    #     avg_rev.append(x)
+    # print(total_revenue)
+    for i in range(len(total_revenue)):
+        if i < (len(total_revenue)-1):
+            avg_rev.append(int(total_revenue[i+1])-int(total_revenue[i]))
+        y = sum(avg_rev)/int(len(avg_rev))
+    print("Average Change in Revenue: ${:.0f}".format(y))
+
+    # Index for total_revenue is 0-40 and index for avg_rev is 0-39
+    # Calculate greatest increase in revenue (date and amount)
+    greatest_increase = max(avg_rev)
+    for z in avg_rev:
+        if z == greatest_increase:
+            max_date = int(avg_rev.index(z)) + 1
+
+            print("Greatest Increase in Revenue: " + str(total_months[max_date]) +
+                  " ($" + str(greatest_increase) + ")")
+
+
+    # Calculate greatest decrease in revenue (date and amount)
+    greatest_decrease = min(avg_rev)
+    for a in avg_rev:
+        if a == greatest_decrease:
+            min_date = int(avg_rev.index(a)) + 1
+
+            print("Greatest Decrease in Revenue: " + str(total_months[min_date]) +
+                  " ($" + str(greatest_decrease) + ")")
---
+++ python-challenge/PyBoss/main.py
@@ -0,0 +1,124 @@
+import os
+import csv
+import re
+from datetime import datetime
+
+
+boss_path = os.path.join("raw_data", "employee_data2.csv")
+output_path = os.path.join("../PyBoss", "solved_like_a_boss_2.csv")
+
+# Create empty lists to store data and manipulate
+emp_id = []
+
+name = []
+first_name = []
+last_name = []
+
+date = []
+new_date = []
+
+ssn = []
+ssn_1 = []
+ssn_2 = []
+ssn_3 = []
+new_ssn = []
+
+state = []
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+state_abbrev = {v: k for k, v in us_state_abbrev.items()}
+new_state = []
+
+with open(boss_path, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvfile, None)
+
+    # The Name column should be split into separate First Name and Last Name columns.
+    for row in csvreader:
+        emp_id.append(row[0])
+        name.append(row[1].split(" "))
+        date.append(row[2])
+        ssn.append(row[3])
+        state.append(row[4])
+
+        for a in name:
+            first_name.append(a[0])
+            last_name.append(a[1])
+
+    # The DOB data should be re-written into DD/MM/YYYY format from YYYY/DD/MM
+    for d in range(len(date)):
+        new_date.append((datetime.strptime(date[d], '%Y-%m-%d').strftime('%d/%m/%Y')))
+
+    # The SSN data should be re-written such that the first five numbers are hidden from view.
+    for c in range(len(ssn)):
+        new_ssn.append(re.sub('\d{3}-\d{2}', '***-**', ssn[c]))
+
+    # The State data should be re-written as simple two-letter abbreviations.
+    for s in range(len(state)):
+        for key, value in state_abbrev.items():
+            if state[s] in value:
+                new_state.append(key)
+
+
+with open(output_path, "w", newline='') as csvfile:
+
+    # Initialize the csv writer
+    boss_writer = csv.writer(csvfile, delimiter=',')
+
+    #Write the first row for column headers
+    boss_writer.writerow(["Emp ID", "First Name", "Last Name", "DOB", "SSN", "State"])
+
+    #Write the second row for info
+    new = zip(emp_id, first_name, last_name, new_date, new_ssn, new_state)
+    for row in new:
+        boss_writer.writerow(row)
